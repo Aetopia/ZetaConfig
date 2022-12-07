0d0f205935821c7c304ada185668c6420d14f62b
Aetopia <kirbymario22@gmail.com>
2022-12-07 13:16:47 +0530

    Update main.c

--- src/WDMT/main.c
+++ src/WDMT/main.c
@@ -3,7 +3,6 @@
 #include <libgen.h>
 #include <psapi.h>
 #include <shellscalingapi.h>
-#include <stdio.h>
 
 // Prototypes
 
@@ -13,16 +12,11 @@ struct WINDOW;
 // Check if the current foreground window is the hooked process' window.
 BOOL IsProcWndForeground(struct WINDOW *wnd);
 
-void SetWndStyle(struct WINDOW *wnd);
-
 // Check for a specific foreground window via its PID and once hooked, set the display mode.
 void CheckForegroundWndPID(struct WINDOW *wnd);
 
-// Check if the hooked process is alive or not.
-void IsProcAlive(struct WINDOW *wnd);
-
 // Check the hooked process is alive.
-void IsWndProcAlive(struct WINDOW *wnd);
+void IsProcAlive(struct WINDOW *wnd);
 
 // Apply the desired resolution when the hooked process is in the foreground.
 void SetForegroundWndDM(struct WINDOW *wnd);
@@ -32,58 +26,32 @@ void ResetForegroundWndDM(struct WINDOW *wnd);
 
 struct WINDOW
 {
-    HWND pwnd, hwnd;   // HWND of the hooked process's window & reserved HWND variable.
-    HANDLE hproc;      // HANDLE to the hooked process.
-    DEVMODE *dm;       // Display mode to be applied when the hooked process' window is in the foreground
-    BOOL reset;        // Reset the display mode back to default.
-    DWORD id, ec, pid; // PID of the hooked process & reserved variables.
-    int cx, cy, x, y;
-    char *monitor, *name; // Name of the monitor, the window is present on & name of the process.
+    HWND pwnd, hwnd;        // HWND of the hooked process' window & reserved HWND variable.
+    HANDLE hproc;           // HANDLE to the hooked process.
+    DEVMODE *dm;            // Display mode to be applied when the hooked process' window is in the foreground
+    BOOL reset;             // Reset the display mode back to default.
+    DWORD process, ec, pid; //  PID of the hooked process & reserved exit code + PID variables. //
+    char *monitor;          // Name of the monitor, the window is present on.
 };
 
 BOOL IsProcWndForeground(struct WINDOW *wnd)
 {
     wnd->hwnd = GetForegroundWindow();
     GetWindowThreadProcessId(wnd->hwnd, &wnd->pid);
-    if (wnd->id == wnd->pid && wnd->hwnd != 0)
+    if (wnd->process == wnd->pid)
     {
         if (wnd->pwnd != wnd->hwnd)
         {
             wnd->pwnd = wnd->hwnd;
-            SetWndBorderless(wnd);
         }
         return FALSE;
     };
     return TRUE;
 }
 
-void SetWndStyle(struct WINDOW *wnd)
-{
-    SetWindowLongPtr(wnd->pwnd, GWL_STYLE,
-                     GetWindowLongPtr(wnd->pwnd, GWL_STYLE) &
-                         ~(WS_OVERLAPPEDWINDOW));
-    SetWindowLongPtr(wnd->pwnd, GWL_EXSTYLE,
-                     GetWindowLongPtr(wnd->pwnd, GWL_EXSTYLE) &
-                         ~(WS_EX_DLGMODALFRAME |
-                           WS_EX_COMPOSITED |
-                           WS_EX_OVERLAPPEDWINDOW |
-                           WS_EX_LAYERED |
-                           WS_EX_STATICEDGE |
-                           WS_EX_TOOLWINDOW |
-                           WS_EX_APPWINDOW |
-                           WS_EX_TOPMOST));
-    SetWindowPos(wnd->pwnd,
-                 0,
-                 wnd->x,
-                 wnd->y,
-                 wnd->cx,
-                 wnd->cy,
-                 SWP_FRAMECHANGED);
-}
-
 void CheckForegroundWndPID(struct WINDOW *wnd)
 {
-    wnd->hproc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, wnd->id);
+    wnd->hproc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, wnd->process);
     if (!wnd->hproc)
     {
         CloseHandle(wnd->hproc);
@@ -95,31 +63,14 @@ void CheckForegroundWndPID(struct WINDOW *wnd)
     }
     do
     {
-        IsProcAlive(wnd);
-    } while (!!IsProcWndForeground(wnd));
-}
-
-void CheckForegroundWndProc(struct WINDOW *wnd)
-{
-    char file[MAX_PATH];
-    for (;;)
-    {
-        wnd->pwnd = GetForegroundWindow();
-        GetWindowThreadProcessId(wnd->pwnd, &wnd->id);
-        wnd->hproc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, wnd->id);
-        GetModuleFileNameEx(wnd->hproc, 0, file, MAX_PATH);
-        if (strcmp(strlwr(basename(file)), wnd->name) == 0)
-        {
-            break;
-        }
-        CloseHandle(wnd->hproc);
-    }
+        Sleep(1);
+    } while (!IsProcWndForeground());
 }
 
 void IsProcAlive(struct WINDOW *wnd)
 {
     GetExitCodeProcess(wnd->hproc, &wnd->ec);
-    if (wnd->ec != STILL_ACTIVE)
+    if (wnd->ec != STILL_ACTIVE || IsWindowVisible(wnd->pwnd))
     {
         CloseHandle(wnd->hproc);
         for (;;)
@@ -135,17 +86,19 @@ void IsProcAlive(struct WINDOW *wnd)
                     ChangeDisplaySettingsEx(wnd->monitor, 0, NULL, 0, NULL);
                     exit(0);
                 };
+            }
+            else
+            {
+                exit(0);
             };
-            exit(0);
         };
-        Sleep(1);
     };
+    Sleep(1);
 }
 
 void SetForegroundWndDM(struct WINDOW *wnd)
 {
     wnd->reset = FALSE;
-    printf("Check!\n");
     do
     {
         IsProcAlive(wnd);
@@ -153,35 +106,33 @@ void SetForegroundWndDM(struct WINDOW *wnd)
     do
     {
         ShowWindow(wnd->pwnd, SW_RESTORE);
-    } while (IsIconic(wnd->pwnd) &&
-             IsWindow(wnd->pwnd));
-    ChangeDisplaySettingsEx(wnd->monitor,
-                            wnd->dm,
-                            NULL,
-                            CDS_FULLSCREEN,
-                            NULL);
-    ResetForegroundWndDM(wnd);
+    } while (IsIconic(wnd->pwnd));
+    if (ChangeDisplaySettingsEx(wnd->monitor,
+                                wnd->dm,
+                                NULL,
+                                CDS_FULLSCREEN,
+                                NULL) == DISP_CHANGE_SUCCESSFUL)
+    {
+        ResetForegroundWndDM(wnd);
+    };
 }
 
 void ResetForegroundWndDM(struct WINDOW *wnd)
 {
     wnd->reset = TRUE;
-    printf("Uncheck!\n");
     do
     {
         IsProcAlive(wnd);
     } while (!IsProcWndForeground(wnd));
     do
     {
-        ShowWindow(wnd->pwnd, SW_MINIMIZE);
-    } while (!IsIconic(wnd->pwnd) &&
-             IsWindow(wnd->pwnd) &&
-             !SetForegroundWindow(FindWindow("Shell_TrayWnd", NULL)));
-    ChangeDisplaySettingsEx(wnd->monitor,
-                            0,
-                            NULL,
-                            CDS_FULLSCREEN,
-                            NULL);
+        if (SetForegroundWindow(FindWindow("Shell_TrayWnd", NULL)))
+        {
+            ShowWindow(wnd->pwnd, SW_MINIMIZE);
+        };
+    } while (!IsIconic(wnd->pwnd));
+
+    ChangeDisplaySettingsEx(wnd->monitor, 0, NULL, CDS_FULLSCREEN, NULL);
     ChangeDisplaySettingsEx(wnd->monitor, 0, NULL, 0, NULL);
     SetForegroundWndDM(wnd);
 }
@@ -206,6 +157,21 @@ int main(int argc, char *argv[])
         return 0;
     };
 
+    // Check if the <PID/Process> argument contains only integers or not.
+    if (strspn(argv[1], "0123456789") == strlen(argv[1]))
+    {
+        wnd.process = atoi(argv[1]);
+        CheckForegroundWndPID(&wnd);
+    }
+    else
+    {
+        MessageBox(0,
+                   "Invaild PID!",
+                   "Window Display Mode Tool",
+                   MB_ICONEXCLAMATION);
+        return 1;
+    };
+
     // Setup the DEVMODE structure.
     dm.dmPelsWidth = atoi(argv[2]);
     dm.dmPelsHeight = atoi(argv[3]);
@@ -223,41 +189,37 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Check if the <PID/Process> argument contains only integers or not.
-    if (strspn(argv[1], "0123456789") == strlen(argv[1]))
-    {
-        wnd.id = atoi(argv[1]);
-        CheckForegroundWndPID(&wnd);
-    }
-    else
-    {
-        wnd.name = strlwr(argv[1]);
-        CheckForegroundWndProc(&wnd);
-    };
-
     // Source: https://devblogs.microsoft.com/oldnewthing/20100412-00/?p=14353
     // Restore the window if its maximized.
-    do
+    if (IsZoomed(wnd.hwnd))
     {
-        ShowWindow(wnd.pwnd, SW_RESTORE);
-    } while (IsZoomed(wnd.pwnd));
+        ShowWindow(wnd.hwnd, SW_RESTORE);
+    };
 
     // Get the monitor, the window is present on.
-    hmon = MonitorFromWindow(wnd.pwnd, MONITOR_DEFAULTTONEAREST);
+    hmon = MonitorFromWindow(wnd.hwnd, MONITOR_DEFAULTTONEAREST);
     GetMonitorInfo(hmon, (MONITORINFO *)&mi);
+    // Name of the monitor, the window is present on.
     wnd.monitor = mi.szDevice;
-    wnd.x = mi.rcMonitor.left;
-    wnd.y = mi.rcMonitor.top;
+    // Get the current resolution of the monitor the window is on.
+    EnumDisplaySettings(mi.szDevice, ENUM_CURRENT_SETTINGS, &cdm);
 
-    // If the specified resolution is not the same as the display native resolution then execute ResetForegroundWndDM(struct WINDOW *wnd).
-    // This code block additionally sizes the window based on the DPI scaling set by the desired display resolution.
-    if (ChangeDisplaySettingsEx(mi.szDevice, wnd.dm, NULL, CDS_FULLSCREEN, NULL) == DISP_CHANGE_SUCCESSFUL)
-    {
-        GetDpiForMonitor(hmon, 0, &dpiX, &dpiY);
-        wnd.cx = dm.dmPelsWidth * (float)dpiC / dpiX;
-        wnd.cy = dm.dmPelsHeight * (float)dpiC / dpiY;
-        SetWndStyle(&wnd);
-        ResetForegroundWndDM(&wnd);
-    };
+    // Set the window style to borderless.
+    SetWindowLongPtr(wnd.hwnd, GWL_STYLE,
+                     GetWindowLongPtr(wnd.hwnd, GWL_STYLE) & ~(WS_OVERLAPPEDWINDOW));
+    SetWindowLongPtr(wnd.hwnd, GWL_EXSTYLE,
+                     GetWindowLongPtr(wnd.hwnd, GWL_EXSTYLE) & ~(WS_EX_OVERLAPPEDWINDOW));
+
+    // This code block sizes the window based on the DPI scaling set by the desired display resolution.
+    ChangeDisplaySettingsEx(wnd.monitor, wnd.dm, NULL, CDS_FULLSCREEN, NULL);
+    GetDpiForMonitor(hmon, 0, &dpiX, &dpiY);
+    SetWindowPos(wnd.hwnd,
+                 HWND_TOP,
+                 mi.rcMonitor.left,
+                 mi.rcMonitor.top,
+                 dm.dmPelsWidth * (float)dpiC / dpiX,
+                 dm.dmPelsHeight * (float)dpiC / dpiY,
+                 SWP_FRAMECHANGED);
+    ResetForegroundWndDM(&wnd);
     return 0;
 }
